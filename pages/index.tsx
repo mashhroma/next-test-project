96765a8f7f08b9b03eb907c66d1937b296e9bb26
mashhroma <mashh@mail.ru>
2024-06-20 13:10:15 +0300

    ready to test finally

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
 import Heading from "@/components/Heading";
 import Socials from "@/components/Socials";
+import { socials } from "./api/data/socials";
 
-export const getStaticProps = async () => {
-  const response = await fetch(`${process.env.API_HOST}/socials/`);
-  const data = await response.json();
+// export const getStaticProps = async () => {
+//   const response = await fetch(`${process.env.API_HOST}/socials/`);
+//   const data = await response.json();
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
 
-  return {
-    props: { socials: data },
-  }
-};
+//   return {
+//     props: { socials: data },
+//   }
+// };
 
-export default function Home({ socials }: { socials: Social[] }) {
+export default function Home() {
   return (
     <main>
       <Heading text='Main page' />
